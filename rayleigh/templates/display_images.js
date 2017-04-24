dcae44e141e3ea966ba3e4bf2aba180e211096bc
QZHehe <htszdm@hotmail.com>
2017-04-24 21:51:30 +0800

    完善分页

--- rayleigh/templates/display_images.js
+++ rayleigh/templates/display_images.js
@@ -5,19 +5,21 @@ var items = $.map(show_results, function(val, i) {
   var url = val['url'];
   var distance = sprintf('%.2f', val['distance']);
   var num = i;
-  var a_search_by_image = sprintf('<a href="%s">search by image</a>',
+  var a_search_by_image = sprintf('<a href="%s">检索此图</a>',
     sprintf('/search_by_image/%s/%s/%s/%s', sic_type, fea_type ,'no', id));
-  var modify_image = sprintf('<a href="%s">modify image</a>',
+  var modify_image = sprintf('<a href="%s">颜色修正</a>',
     sprintf('/modify_image/%s/%s', sic_type, id));
   var a_id = sprintf('<a>%s</a>',id);
   var a_num = sprintf('<a>%s</a>',num.toString());
-  var img = sprintf('<img src="%s" alt="%.3f" width="%d" height="%d" />',
-    '/static/'+url, val['distance'], val['width']/2, val['height']/2);
+  // var img = sprintf('<img src="%s" alt="%.3f" width="%d" height="%d" class="img-thumbnail" />',
+  //   '/static/'+url, val['distance'], val['width']/2, val['height']/2);
+  var img = sprintf('<img src="%s" alt="%.3f" width="150px" class="img-thumbnail" />',
+    '/static/'+url, val['distance']);
   var img_link = sprintf('<a href=%s>%s</a>', url, img);
-  var caption = [a_num,distance, a_search_by_image,a_id,modify_image].join(' | ');
+  var caption = [a_search_by_image,modify_image].join(' | ');
   return sprintf('<div class="result">%s<br />%s</div>', img_link, caption)
 });
 
-var time = sprintf('Time elapsed: %.2f ms.<br />', json_data['time_elapsed']*1000);
+var time = sprintf('索引时间: %.2f ms.<br />', json_data['time_elapsed']*1000);
 
 $('#results').html(time+items.join(''));
